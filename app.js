a5f14d710e100a059b5f5df7f65ce7815ceecc6f
Akhil Ramesh <akkhil@outlook.com>
2015-04-26 19:02:15 +0530

    stable version

--- app.js
+++ app.js
@@ -25,9 +25,8 @@ window.addEventListener('load', function(){
 function show(e){
      if(isChar(e))
      {
-      var dropdown = document.getElementsByClassName("dropdown");
+      var dropdown = document.getElementsByClassName("dropdown")[0];
       var box = document.getElementById('input');
-      dropdown = dropdown[0];
       dropdown.style.display = 'block';
       box.style.borderRadius = '3px 3px 0 0';
      }
@@ -35,8 +34,7 @@ function show(e){
 function hide(e){
   if(e.which == 8)
   {
-  var dropdown = document.getElementsByClassName("dropdown");
-  dropdown=dropdown[0];
+  var dropdown = document.getElementsByClassName("dropdown")[0];
   var box = document.getElementById('input');
   str=box.value;
   if(str==="")
@@ -46,7 +44,14 @@ function hide(e){
 }
 //function to find match from JSON data
 function findMatch(e){
+  if(!e.altKey && !e.ctrlKey && !e.metaKey)
+  {
   var box = document.getElementById('input');
+  var dropdown = document.getElementsByClassName('dropdown')[0];
+
+  while(dropdown.firstChild) // to clear the dropdown <li> for each query
+    dropdown.removeChild(dropdown.firstChild);
+
   var str=box.value;
   var result=[];
   if(str!="") {
@@ -55,10 +60,29 @@ function findMatch(e){
       if(user.username.contains(str) || user.email.contains(str))
         result.push(user);
     }
-    if(result.length>0)
+    if(result.length>0) {
       console.log(result.length);
-    else
+      var ul = document.createElement("ul");
+      dropdown.appendChild(ul);
 
+      for(var i=0;i<result.length;i++)
+      { var li = document.createElement("li");
+        var a  = document.createElement("a");
+        var p  = document.createElement("p");
+        //sorry i'm a js rookie. there's a lot to refactor i'm sure about that.
+        var username = result[i].username.toLowerCase();
+        li.innerHTML = "@"+username;
+        p.innerHTML = result[i].email.toLowerCase();
+        a.appendChild(li);
+        a.appendChild(p);
+        a.setAttribute("href", "#");
+        ul.appendChild(a);
+      }
+      }
+      else {
+      dropdown.innerHTML = "<li style='color: #477DCA;'> Can't find any results </li>"
+      }
+    }
   }
 }
 
@@ -72,5 +96,5 @@ String.prototype.contains=function(str){
 
 function isChar(evt) {
   if(typeof evt.which == "number" && evt.which > 0)
-    return !evt.ctrlKey && !evt.metaKey && !evt.altKey && evt.which != 8;
+    return !evt.ctrlKey && !evt.metaKey && !evt.altKey && evt.which != 8 && evt.which !=9;
 }
